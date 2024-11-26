a0036a9bd76e60d8a3d62a66915c4b7157d3c76a
A-Awal <103506955+A-Awal@users.noreply.github.com>
2024-11-26 10:08:34 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -13,7 +13,7 @@ var server = http.createServer(function(request, response){
         request.on('data', function(chunk){
             body += chunk;
         });
-        request.on('end', function(){
+        request.on('ed', function(){
             var comment = querystring.parse(body);
             comments.push(comment);
             response.writeHead(200, {'Content-Type': 'text/plain'});
